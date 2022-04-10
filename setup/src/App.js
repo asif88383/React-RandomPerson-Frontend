4e5e4212cd22ce1d169dfee5f794ff942e2fcefe
Muhammad Asif <asif88383@gmail.com>
2022-04-10 18:00:06 +0500

    first commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -7,9 +7,35 @@ import {
   FaPhone,
   FaLock,
 } from 'react-icons/fa'
+
 const url = 'https://randomuser.me/api/'
 const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [person, setPerson] = useState(null)
+  const [value, setValue] = useState('random person')
+  const [title, setTitle] = useState('name')
+
+  const getPerson = async () => {
+    setLoading(true)
+    const response = await fetch(url)
+    const data = await response.json()
+    const person = data.results[0]
+    const { phone, email } = person
+    const { large: image } = person.picture
+    const { password } = person.login
+    const { first, last } = person.name
+    const {
+      dob: { age },
+    } = person
+    const {
+      street: { number, name },
+    } = person.location
+
+    
+
+
   return <h2>random user starter</h2>
 }
 
